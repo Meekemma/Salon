a332f695e175782878a73916096fa27e710659f7
unknown <meekycute007@yahoo.com>
2024-12-11 18:22:22 +0100

    first commit

--- src/components/CustomeAbout.jsx
+++ src/components/CustomeAbout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FaBook, FaThumbsUp, FaHeart, FaCut } from 'react-icons/fa'; // Correct import for scissors-like icon
-import angel from '../assets/images/Angel.jpg';
-import angelwhite from '../assets/images/angelwhite.jpg'; 
+import Angel from '../assets/images/Angel.jpg';
+import Angelwhite from '../assets/images/Angelwhite.jpg'; 
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import '../styles/main.css';
@@ -11,7 +11,7 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 import Spinner from "./Spinner"; 
 
 const CustomeAbout = () => {
-  const images = [angel, angelwhite]; 
+  const images = [Angel, Angelwhite]; 
 
   const benefits = [
     'Expert professionals with years of experience.',
