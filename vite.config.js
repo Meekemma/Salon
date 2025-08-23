debf2dff44eaf7f06a327924c4f147166b89274d
meekemma <meekycute007@yahoo.com>
2025-08-23 17:21:38 +0100

    updated the vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:3000,
-  }
+  server: {
+    port: 3000,
+  },
+  build: {
+    chunkSizeWarningLimit: 2000, // increase limit to 2000 KB (2 MB)
+  },
 })
